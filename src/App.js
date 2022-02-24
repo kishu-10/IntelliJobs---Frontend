3cfcd31f62f00e2d90c478ce09c19bf78ec49bf3
Kishu Maharjan <kishu.maharjan@infodevelopers.com.np>
2022-02-24 17:18:57 +0545

    fix: add router in index js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./css/style.css";
 import "./css/1.style.css";
 
 function App() {
   return (
-      <Router>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
-      </Router>
   );
 }
 
