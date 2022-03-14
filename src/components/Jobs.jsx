68d7de8687b46196765563ff4d5ae8f040cb8d49
Kishu Maharjan <kishu.maharjan@infodevelopers.com.np>
2022-03-14 15:47:58 +0545

    feat: jobs page design

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -3,11 +3,46 @@ import Footer from "./Footer";
 import Navbar from "./Navbar";
 import avatar from "../assets/avatar.png";
 import { Icon } from "@iconify/react";
+import image from "../assets/post-image.png";
 
 const Jobs = () => {
   return (
     <>
       <Navbar />
+      <div className="row">
+        <div className="job-detail-head">
+          <img className="img-fluid" src={image} alt="" />
+          <div class="col-lg-8">
+            <div className="job-title-div">
+              <div className="job-title-desc">
+                <h1>Your Dream Job is Waiting ...</h1>
+                <p>Search Between More Than 50,000 Open Jobs</p>
+              </div>
+            </div>
+            <div className="row job-filter-row">
+              <div className="col-md-3 job-filter-col">
+                <input type="text" placeholder="Job Title" />
+              </div>
+              <div className="col-md-3 job-filter-col">
+                <input type="text" placeholder="Job Address" />
+              </div>
+              <div className="col-md-3 job-filter-col">
+                <select name="job-category">
+                <option value="" disabled selected>Select Job Category</option>
+                    <option value="it">Information Technology</option>
+                    <option value="it">Information Technology</option>
+                    <option value="it">Information Technology</option>
+                </select>
+              </div>
+              <div className="col-md-3 job-filter-col">
+                <button className="btn btn-primary" type="submit">
+                  Find Job
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="row--grey">
         <div className="container container--grey col-lg-8">
           <div className="grid grid-list">
