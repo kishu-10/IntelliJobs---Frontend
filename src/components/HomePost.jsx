94e9ad94c1762353fdc44a553fac75b8247d1c67
Kishu Maharjan <kishu.maharjan@infodevelopers.com.np>
2022-04-11 21:53:45 +0545

    feat: homepage

--- src/components/HomePost.jsx
+++ src/components/HomePost.jsx
@@ -1,44 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import avatar from "../assets/avatar.png";
-import image from "../assets/post-image.png";
+import { useEffect } from "react";
+import axios from "axios";
+import { Skeleton } from "@mantine/core";
 
 const HomePost = () => {
-  return (
-    <div className="container container--mid col-lg-5 col-md-6 col-6">
-      <div className="feeds-posts-container">
-        <div className="row post-author-row">
-          <div className="col-lg-2">
-            <img src={avatar} height="70" alt="" />
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const result = await axios.get(
+          `http://127.0.0.1:8000/api/feeds/posts/`,
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        setPosts(result.data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchUser();
+  }, []);
+  if (posts) {
+    return (
+      <div className="container container--mid col-lg-5 col-md-6 col-6">
+        {posts.map((post, index) => (
+          <div className="feeds-posts-container" key={index}>
+            <div className="row post-author-row">
+              <div className="col-lg-2">
+                <img
+                  src={post.display_picture ? post.display_picture : avatar}
+                  height="70"
+                  alt=""
+                />
+              </div>
+              <div className="col-lg-10 col-md-9 col-6">
+                <p className="font-weight-bold">{post.author_name}</p>
+                <small className="text-muted font-12">{post.date_updated}</small>
+                <p>{post.caption}</p>
+              </div>
+            </div>
+            <img
+              className="feeds-post-image"
+              src={post.image}
+              height="90"
+              alt=""
+            />
           </div>
-          <div className="col-lg-10 col-md-9 col-6">
-            <p className="font-weight-bold">Kishu Maharjan</p>
-            <small className="text-muted">Feb 28, 2022</small>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry.
-            </p>
-          </div>
-        </div>
-        <img className="feeds-post-image" src={image} height="90" alt="" />
+        ))}
       </div>
-      <div className="feeds-posts-container">
-        <div className="row post-author-row">
-          <div className="col-lg-2">
-            <img src={avatar} height="70" alt="" />
-          </div>
-          <div className="col-lg-10 col-md-9 col-6">
-            <p className="font-weight-bold">Kishu Maharjan</p>
-            <small className="text-muted">Feb 28, 2022</small>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry.
-            </p>
-          </div>
-        </div>
-        <img className="feeds-post-image" src={image} height="90" alt="" />
-      </div>
-    </div>
-  );
+    );
+  }
+  return <Skeleton height={8} radius="xl" />
 };
 
 export default HomePost;
