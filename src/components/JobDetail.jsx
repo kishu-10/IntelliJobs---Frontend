49971ab41e31580420d032386a787a04aaca0fa0
Kishu Maharjan <kishu.maharjan@infodevelopers.com.np>
2022-04-12 15:21:57 +0545

    feat: job apply

--- src/components/JobDetail.jsx
+++ src/components/JobDetail.jsx
@@ -2,13 +2,18 @@ import Footer from "./Footer";
 import Navbar from "./Navbar";
 import { Icon } from "@iconify/react";
 import axios from "axios";
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback, useMemo } from "react";
 import { useParams } from "react-router-dom";
 import { showError, showSuccess } from "../utils/toast";
+import { Modal } from "@mantine/core";
 
 const JobDetail = () => {
+  const userId = useMemo(() => localStorage.getItem("userId"), []);
   const { jobId } = useParams();
   const [job, setJob] = useState(null);
+  const [opened, setOpened] = useState(false);
+  const [cvs, setCvs] = useState([]);
+  const [selected, setSelected] = useState(null);
 
   const fetchJob = useCallback(async () => {
     try {
@@ -30,6 +35,38 @@ const JobDetail = () => {
     fetchJob();
   }, [fetchJob]);
 
+  const fetchCV = async () => {
+    try {
+      const result = await axios.get(
+        `http://127.0.0.1:8000/api/users/candidate-cv/${userId}/`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      setCvs(result.data.data);
+    } catch (error) {
+      console.log(error);
+    }
+    console.log(cvs);
+  };
+
+  const removeClass = () => {
+    var elements = document.querySelectorAll(".btn-cv");
+    [].forEach.call(elements, function (el) {
+      el.classList.add("btn-outline-primary");
+      el.classList.remove("btn-primary");
+    });
+  };
+  const selectCv = (index) => {
+    setSelected(index);
+    const element = document.getElementById(index);
+    removeClass();
+    element.classList.add("btn-primary");
+    element.classList.remove("btn-outline-primary");
+  };
+
   const addToWishlist = () => {
     const formData = new FormData();
     formData.append("job", jobId);
@@ -48,9 +85,81 @@ const JobDetail = () => {
       });
   };
 
+  const applyForJob = () => {
+    const jsonData = {
+      cv: selected,
+      job: jobId,
+    };
+    if (!selected) {
+      showError("CV must be selected.");
+    } else {
+      console.log(jsonData);
+      axios({
+        method: "post",
+        url: `http://127.0.0.1:8000/api/jobs/apply/`,
+        data: jsonData,
+        headers: { "Content-Type": "application/json" },
+      })
+        .then((response) => {
+          showSuccess("Applicaiton sent successfully.");
+          fetchJob();
+          setOpened(false);
+        })
+        .catch((error) => {
+          console.log(error.response);
+          showError(
+            error.response.data.data.message
+              ? error.response.data.data.message[0]
+              : "Error"
+          );
+        });
+    }
+  };
+
   if (job) {
     return (
       <>
+        <Modal
+          size="60%"
+          centered
+          opened={opened}
+          onClose={() => setOpened(false)}
+          title={`Apply For ${job.title}?`}
+        >
+          <hr />
+          <p className="text-primary font-weight-bold mb-2 ml-2">
+            Select your CV
+          </p>
+          <div className="row">
+            {cvs.map((cv) => (
+              <button
+                key={cv.id}
+                id={cv.id}
+                className="btn btn-cv btn-outline-primary col-md-3 ml-3 mb-3"
+                onClick={() => selectCv(cv.id)}
+              >
+                <div className="align-vertical justify-content-center pt-2 pb-2">
+                  <p className="font-14">
+                    <Icon
+                      icon="icomoon-free:files-empty"
+                      className="mr-2"
+                      fontSize={20}
+                    />
+                    {cv.file_name}
+                  </p>
+                </div>
+              </button>
+            ))}
+          </div>
+          <div className="col-md-9 offset-md-10 pl-5">
+            <button
+              className="btn btn-primary ml-4"
+              onClick={() => applyForJob()}
+            >
+              Apply
+            </button>
+          </div>
+        </Modal>
         <Navbar />
         <div className="row">
           <div className="job-detail-head">
@@ -60,9 +169,7 @@ const JobDetail = () => {
                 <div className="job-title-desc">
                   <h1>{job.title}</h1>
                   <h3 className="mt-3">{job.organization}</h3>
-                  <p className="text-justify">
-                   {job.org_description}
-                  </p>
+                  <p className="text-justify">{job.org_description}</p>
                 </div>
               </div>
             </div>
@@ -124,7 +231,21 @@ const JobDetail = () => {
                     style={{ cursor: "pointer", color: "var(--primary)" }}
                     onClick={() => addToWishlist()}
                   />
-                  <button className="btn btn-primary ml-4">Apply Now</button>
+                  {job.has_applied ? (
+                    <button className="btn btn-primary ml-4" disabled>
+                      Applied
+                    </button>
+                  ) : (
+                    <button
+                      className="btn btn-primary ml-4"
+                      onClick={() => {
+                        setOpened(true);
+                        fetchCV();
+                      }}
+                    >
+                      Apply Now
+                    </button>
+                  )}
                 </div>
               </div>
             </div>
@@ -170,7 +291,7 @@ const JobDetail = () => {
     );
   }
 
-  return <>Loading ....</>;
+  return <></>;
 };
 
 export default JobDetail;
